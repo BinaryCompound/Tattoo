9999b9c9548f5085b011bcd97b4ee17a903d6470
unknown <BinaryCompound2@gmail.com>
2023-05-08 11:54:11 +0300

    add button on top

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").classList.add("show");
+  } else {
+    document.getElementById("scrollToTopBtn").classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
+
