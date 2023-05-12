2c23588168bddab292ce9ebf7383676bf4e38c75
unknown <BinaryCompound2@gmail.com>
2023-05-12 09:55:56 +0300

    addited react

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -9,7 +9,34 @@ function scrollFunction() {
 }
 
 function scrollToTop() {
-  document.body.scrollTop = 0; // Для Safari
+  document.body.scrollTop = 0; // Для Safaris
   document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
 }
+
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
 
